ca9bb043e72dcd811ade1a71d78f151941bf71de
Henrique Domiciano <henriquedomiciano@yahoo.com>
2024-11-04 22:10:42 -0300

    Added configuration file

--- parameters.py
+++
@@ -1,5 +0,0 @@
-user = "hdoarkade@gmail.com"
-password = ""
-
-SMTP_servername = "smtp.gmail.com"
-port = 587
--- send_mail.py
+++ send_mail.py
@@ -5,9 +5,53 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 import smtplib
-import parameters
 import argparse
 import os
+import configparser
+import inquirer
+
+class Configuration():
+    def __init__(self,user,password,port,SMTP_servername):
+        self.user = user
+        self.password = password
+        self.port = port
+        self.SMTP_servername = SMTP_servername
+    
+    def load_config(self,config_path):
+        full_path_name = os.path.join(os.path.dirname(__file__),config_path)
+        conf = configparser.ConfigParser()
+        with open(full_path_name,'r') as f:
+            conf.read_file(f)
+        try:
+            self.password = conf["DEFAULT"]['password']
+            self.user = conf["DEFAULT"]['user']
+            self.port = conf["DEFAULT"]['port']
+            self.SMTP_servername = conf["DEFAULT"]['SMTP_servername']
+        except:
+            print("Something went wrong loading configuration")
+    
+    def write_config(self,config_path):
+        full_path_name = os.path.join(os.path.dirname(__file__),config_path)
+        conf = configparser.ConfigParser()
+        with open(full_path_name,'r') as f:
+            conf.read_file(f)
+        questions = [
+            inquirer.Text('user', message="What's your email"),
+            inquirer.Text('password', message="What's your email password (app password)"),
+            inquirer.Text('SMTP_servername', message="SMTP server"),
+            inquirer.Text("port",message="The SMTP server port")
+        ]
+
+        results = inquirer.prompt(questions)
+
+        results['port'] = int(results.get('port'))
+        conf.read_dict({"DEFAULT":results})
+        print("Configuration saved sucessfully")
+        with open(full_path_name,"w") as f:
+            conf.write(f)
+
+        #tomllib.load()
+        
 
 __author__ = "Henrique Domiciano <henriquedomiciano@yahoo.com>"
 __program__ = "send_mail"
@@ -16,6 +60,8 @@ __program__ = "send_mail"
 usage = """
 Used to send email messages, to be able to send email just change the parameters.py change the user and password, or the smtp server
 you can also send attachments, messages etc. 
+
+If running first time run with -config as the only option to configure. 
 """
 
 
@@ -32,13 +78,23 @@ args.add_argument("-s", "--subject")
 args.add_argument("-m", "--message")
 args.add_argument("-a", "--attachment", nargs="+", default=[])
 args.add_argument("-v", "--verbose", action="store_true")
-
+args.add_argument('-c','--config',action='store_true')
 argum = args.parse_args()
 
+if argum.config:
+    c = Configuration(None,None,None,None)
+    c.write_config("parameters.config")
+    c.load_config("parameters.config")
+    exit()
+
+
 send_to = argum.to
+c = Configuration(None,None,None,None)
+c.load_config('parameters.config')
+log(argum.verbose,"Configuration loaded")
 
 msg = MIMEMultipart()
-msg["From"] = parameters.user
+msg["From"] = c.user
 msg["To"] = COMMASPACE.join(send_to)
 msg["Date"] = formatdate(localtime=True)
 msg["Subject"] = argum.subject
@@ -60,13 +116,13 @@ for f in argum.attachment:
     msg.attach(part)
 
 if __name__ == "__main__":
-    with smtplib.SMTP(parameters.SMTP_servername, parameters.port) as smtp:
+    with smtplib.SMTP(c.SMTP_servername, c.port) as smtp:
         try:
             smtp.ehlo()
             smtp.starttls()
             smtp.ehlo()
-            smtp.login(user=parameters.user, password=parameters.password)
-            mail = smtp.sendmail(parameters.user, send_to, msg.as_string())
+            smtp.login(user=c.user, password=c.password)
+            mail = smtp.sendmail(c.user, send_to, msg.as_string())
             if mail == {} and argum.verbose:
                 print(f"[INFO] mail sent to {(' '.join(send_to))}")
         except Exception as e:
